cd869272ddf0c918253e498b435c20e1e3f94455
courbisWeb <courbisweb@gmail.com>
2021-09-09 00:31:34 -0300

    First template created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { CommodityListComponent } from './components/commodity-list/commodity-list.component';
 import { FormsModule } from '@angular/forms';
 import { CashRegisterComponent } from './components/cash-register/cash-register.component';
+import { PaymentMethodComponent } from './components/payment-method/payment-method.component';
+import { AmountsComponent } from './components/amounts/amounts.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { CashRegisterComponent } from './components/cash-register/cash-register.
     DateComponent,
     HeaderComponent,
     CommodityListComponent,
-    CashRegisterComponent
+    CashRegisterComponent,
+    PaymentMethodComponent,
+    AmountsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/amounts/amounts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-amounts',
+  templateUrl: './amounts.component.html',
+  styleUrls: ['./amounts.component.css']
+})
+export class AmountsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
